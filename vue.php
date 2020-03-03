2e597ee5600b518cea4042a4ea75a5afb269fba7
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-03 15:56:23 +0100

    add dynamic standings

--- vue.php
+++ vue.php
@@ -24,78 +24,22 @@
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>1</td>
-            <th scope="row">Liverpool</th>
-            <td>4</td>
-            <td>12</td>
-            <td>4</td>
-            <td>0</td>
-            <td>0</td>
-            <td>10</td>
-            <td>0</td>
-            <td>10</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <th scope="row">Manchester City</th>
-            <td>4</td>
-            <td>10</td>
-            <td>3</td>
-            <td>0</td>
-            <td>1</td>
-            <td>12</td>
-            <td>4</td>
-            <td>8</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <th scope="row">Chelsea</th>
-            <td>4</td>
-            <td>8</td>
-            <td>2</td>
-            <td>0</td>
-            <td>2</td>
-            <td>9</td>
-            <td>5</td>
-            <td>4</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <th scope="row">Tottenham</th>
-            <td>4</td>
-            <td>7</td>
-            <td>2</td>
-            <td>1</td>
-            <td>1</td>
-            <td>7</td>
-            <td>7</td>
-            <td>0</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <th scope="row">Manchester United</th>
-            <td>4</td>
-            <td>2</td>
-            <td>1</td>
-            <td>2</td>
-            <td>1</td>
-            <td>2</td>
-            <td>6</td>
-            <td>-4</td>
-        </tr>
-        <tr>
-            <td>6</td>
-            <th scope="row">Arsenal</th>
-            <td>4</td>
-            <td>0</td>
-            <td>0</td>
-            <td>4</td>
-            <td>0</td>
-            <td>2</td>
-            <td>12</td>
-            <td>-10</td>
-        </tr>
+        <?php $i=1; ?>
+        <?php foreach ($standings as $team => $teamStats): ?>
+            <tr>
+                <td><?= $i++; ?></td>
+                <th scope="row"><?= $team ?></th>
+                <td><?= $teamStats['games']; ?></td>
+                <td><?= $teamStats['points']; ?></td>
+                <td><?= $teamStats['wins']; ?></td>
+                <td><?= $teamStats['losses']; ?></td>
+                <td><?= $teamStats['draws']; ?></td>
+                <td><?= $teamStats['GF']; ?></td>
+                <td><?= $teamStats['GA']; ?></td>
+                <td><?= $teamStats['GD']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+
         </tbody>
     </table>
 </section>
@@ -114,7 +58,8 @@
         <tbody>
         <?php foreach ($matches as $match): ?>
             <tr>
-                <td><?= (new DateTime($match['match-date'], new DateTimeZone('Europe/Brussels')))->format('M l jS, Y') ?></td>
+                <td><?= (new DateTime($match['match-date'],
+                        new DateTimeZone('Europe/Brussels')))->format('M l jS, Y') ?></td>
                 <td><?= $match['home-team'] ?></td>
                 <td><?= $match['home-team-goals'] ?></td>
                 <td><?= $match['away-team-goals'] ?></td>
