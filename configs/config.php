b9447e3c6ed9b2ee1dc5ee2b415b54f192610380
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-19 20:22:16 +0100

    Update standings according to db migration

--- configs/config.php
+++ configs/config.php
@@ -1,2 +1,5 @@
 <?php
 define('DB_PATH', $_SERVER['DOCUMENT_ROOT'].'/data/scores.sqlite');
+define('TODAY',
+    (new DateTime('now', new DateTimeZone('Europe/Brussels')))
+        ->format('M jS, Y'));
