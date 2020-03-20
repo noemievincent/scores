62390710cea0ab40542695c69178c729e45d0fdd
Dominique Vilain <dominique.vilain@hepl.be>
2020-03-20 16:24:01 +0100

    Add Carbon support using Composer

--- configs/config.php
+++ configs/config.php
@@ -1,5 +1,4 @@
 <?php
 define('DB_PATH', $_SERVER['DOCUMENT_ROOT'].'/data/scores.sqlite');
 define('TODAY',
-    (new DateTime('now', new DateTimeZone('Europe/Brussels')))
-        ->format('M jS, Y'));
+    \Carbon\Carbon::now('Europe/Brussels')->locale('fr_BE')->isoFormat('dddd DD MMMM YYYY'));
