45c44d2192b72cadf6039b6cb40be077ba36a6ea
Dominique Vilain <dominique.vilain@hepl.be>
2020-04-09 08:12:41 +0200

    delete basic register functionality
    Problem with the video, let’s redo it !

--- controllers/Register.php
+++ controllers/Register.php
@@ -4,30 +4,12 @@
 namespace Controllers;
 
 
-use Models\User;
-
 class Register
 {
-    public function create()
+    function create()
     {
         $view = './views/register/create.php';
 
         return compact('view');
-    }
-
-    public function store()
-    {
-        //Collecte et validation des données
-        //À vous pour la validation !
-        //⚠️ ceci est très très imprudent ! Il faut valider les données avant !!
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-
-        //Sauvegarde de l’utilisateur
-        $userModel = new User();
-        $userModel->save(compact('name', 'email', 'password'));
-        header('Location: index.php');
-        exit();
     }
 }
--- models/User.php
+++ models/User.php
@@ -14,19 +14,4 @@ class User extends Model
 
         return $pdoSt->fetch();
     }
-
-    function save(array $user)
-    {
-        try {
-            $insertUserRequest = 'INSERT INTO users(`name`, `email`, `password`) VALUES (:name, :email, :password)';
-            $pdoSt = $this->pdo->prepare($insertUserRequest);
-            $pdoSt->execute([
-                ':name' => $user['name'],
-                ':email' => $user['email'],
-                ':password' => $user['password']
-            ]);
-        } catch (\PDOException $e) {
-            die($e->getMessage());
-        }
-    }
 }
